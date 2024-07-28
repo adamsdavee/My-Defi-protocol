b649149b65f903eb6800176ef96faec13f509bf1
adamsdavee <davidkenechukwu591@gmail.com>
2024-07-28 02:58:03 +0100

    updated

---
+++ contracts/CherryToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CherryToken is ERC20, AccessControl {
+    constructor() ERC20("CHERRYCOIN", "CYN") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
---
+++ deploy/00-deploy-cherry-and-apple-tokens.js
@@ -0,0 +1,29 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  log("Deploying contract..................");
+  const cherryToken = await deploy("CherryToken", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+  log(`Contract deployed at: ${cherryToken.address}`);
+
+  log("--------------------------------------------");
+  log("Deploying contract..................");
+  const appleToken = await deploy("AppleToken", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+  log(`Contract deployed at: ${appleToken.address}`);
+
+  //   if (!(chainId == 31337) && process.env.ETHERSCAN_API_KEY) {
+  //     //verify
+  //     await verify(fundMe.address, [ethUsdPriceAddress]);
+  //     log("Verified...........");
+  //   }
+};
+
+module.exports.tags = ["all", "fund"];
